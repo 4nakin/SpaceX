74a2523e72fe812da64b6b0a319e643f9eb998fc
sukha <sukhanov87@gmail.com>
2019-11-25 10:41:04 +0300

    repository(a few fixes)

--- app/src/main/java/team/lf/spacex/repository/SpaceXRepository.kt
+++ app/src/main/java/team/lf/spacex/repository/SpaceXRepository.kt
@@ -11,20 +11,18 @@ import team.lf.spacex.network.SpaceXApi
 import team.lf.spacex.network.SpaceXApiService
 import team.lf.spacex.network.asDatabaseModels
 
-class SpaceXRepository(private val database: SpaceXDatabase){
+class SpaceXRepository(private val database: SpaceXDatabase) {
 
-
-    val allLaunches : LiveData<List<Launch>> =
-        Transformations.map(database.SpaceXDao.getAllLaunches()){
+    val allLaunches: LiveData<List<Launch>> =
+        Transformations.map(database.SpaceXDao.getAllLaunches()) {
             it.asDomainModels()
         }
 
-    suspend fun refreshAllLaunches(){
-        withContext(Dispatchers.IO){
+    suspend fun refreshAllLaunches() {
+        withContext(Dispatchers.IO) {
             val allLaunches = SpaceXApi.retrofitService.getLaunchesAsync().await()
             database.SpaceXDao.insertAll(allLaunches.asDatabaseModels())
         }
     }
-
 
 }
