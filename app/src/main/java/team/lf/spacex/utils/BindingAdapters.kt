c90f39f75ad25fa5c37c059bc33ee4a692dfa543
sukha <sukhanov87@gmail.com>
2019-12-23 09:43:05 +0300

    ripple

--- app/src/main/java/team/lf/spacex/utils/BindingAdapters.kt
+++ app/src/main/java/team/lf/spacex/utils/BindingAdapters.kt
@@ -1,6 +1,5 @@
 package team.lf.spacex.utils
 
-import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.net.toUri
@@ -8,14 +7,9 @@ import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.target.Target
 import team.lf.spacex.R
-import timber.log.Timber
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -101,5 +95,7 @@ fun Long.dateToString(): String {
     val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
     return sdf.format(date)
 }
+
+
 
 
