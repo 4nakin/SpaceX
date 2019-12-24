f81841b28ffec36f03fac807c6cbd15f9236e3f8
sukha <sukhanov87@gmail.com>
2019-12-24 09:07:52 +0300

    unit test

--- app/src/test/java/team/lf/spacex/utils/BindingAdaptersKtTest.kt
+++ app/src/test/java/team/lf/spacex/utils/BindingAdaptersKtTest.kt
@@ -11,6 +11,13 @@ class BindingAdaptersKtTest {
     fun shortText_returnsShortText() {
         val testString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a aliquet urna. Sed lacus est, vehicula at laoreet vitae, pharetra at justo. Nullam erat erat, aliquam id lacus in, condimentum imperdiet nibh. Phasellus eget nunc auctor, iaculis ex sit amet, vehicula est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a sollicitudin orci, eget facilisis mauris. Sed volutpat sem sed erat hendrerit iaculis."
         val shortText = shortText(20,testString)
-        assertEquals(shortText.length, 23) /// + "..."
+        assertEquals(shortText.length, 23)
+    }
+
+    @Test
+    fun dateToString_returnsStringDate() {
+        val longDate:Long = 1504792200
+        assertEquals(longDate.dateToString(), "07/09/2017")
+
     }
 }
